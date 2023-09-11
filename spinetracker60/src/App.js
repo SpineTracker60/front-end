11c5778aad0d00aa097ae1412c7c4b2ac28952a6
anichan <qufekf97@gmail.com>
2023-09-11 16:15:10 +0900

    얼굴인식 추가

--- spinetracker60/src/App.js
+++ spinetracker60/src/App.js
@@ -1,11 +1,92 @@
+import Webcam from "react-webcam";
+import React, { useRef, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/face-landmarks-detection";
+import { drawMesh } from "./utilities";
+import "./styles.css";
 
+let detector;
 
-function App() {
+export default function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runFacemesh = async () => {
+    const net = facemesh.SupportedModels.MediaPipeFaceMesh;
+    const detectorConfig = {
+      runtime: "tfjs"
+    };
+    detector = await facemesh.createDetector(net, detectorConfig);
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const estimationConfig = { flipHorizontal: false };
+      const faces = await detector.estimateFaces(video, estimationConfig);
+      console.log(faces);
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(() => {
+        drawMesh(faces, ctx);
+      });
+    }
+  };
+
+  useEffect(() => {
+    runFacemesh();
+  }, []);
+
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480
+          }}
+        />
+      </header>
     </div>
   );
 }
-
-export default App;
