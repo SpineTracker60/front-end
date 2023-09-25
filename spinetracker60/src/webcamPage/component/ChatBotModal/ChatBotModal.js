0dc547e14049f616e6e2a9a2f705303f337c55b1
anichan <qufekf97@gmail.com>
2023-09-26 00:00:04 +0900

    챗봇 설정

--- spinetracker60/src/webcamPage/component/ChatBotModal/ChatBotModal.js
+++ spinetracker60/src/webcamPage/component/ChatBotModal/ChatBotModal.js
@@ -1,11 +1,19 @@
-import { useRef } from "react";
+import { useRef , useState, useEffect} from "react";
 import style from "./ChatBotModal.module.css";
 import axios from "axios";
 import { API_BASE_URL_SOCKET } from "../../../constants";
 import { motion } from "framer-motion";
 
+
+
+
+
 function ChatBotModal(props){
     const chat = useRef(null);
+
+    console.log('props.chatList : ',props.chatList);
+
+    const [submitChatState, setSubmitChatState] = useState(false);
 
     const submitChat = () => {
         axios.post(API_BASE_URL_SOCKET + '/chat?memberId='+ props.user.id,
@@ -14,7 +22,12 @@ function ChatBotModal(props){
             "chatTag" : "chat"
         });
         chat.current.value = '';
+        
     }
+    useEffect(() => {
+        // setChatState(botChat);
+        // console.log(chatState);
+    },[props.chatList])
 
     return(
         <div className={style.frame}>
@@ -24,7 +37,43 @@ function ChatBotModal(props){
                 <p className={style.chatBotIntro}>AI 챗봇 레톤이에게 무엇이든 물어보세요!</p>
             </div>
             <div className={style.contentsFrame}>
-
+                <div className={style.chatBotChatDiv}>
+                    <div className={style.chatExample}>
+                    <img src="/img/chatchat.png" className={style.chatBotChatIcon}/>
+                    <div className={style.chatBotChatting}>
+                        <p>
+                            안녕하세요! 저는 척추의 요정 AI 챗봇 레톤
+                            이에요. 궁금하신 게 있다면 저에게 질문해 
+                            주세요. 무엇을 도와드릴까요?<br/>
+                            척추의 요정 소개 🧚<br/>
+                            서비스 이용 안내 🙏<br/>
+                            맞춤 상품 추천 🩻<br/>
+                            바른 자세 안내 📌<br/>
+                            스트레칭 방법 안내 💪<br/>
+                            자유로운 채팅
+                        </p>
+                    </div>
+                    </div>
+                    <ul>
+                    {props.chatList.map((chat) => (
+                        <li>
+                            <div className={style.chattingArea}>
+                                <div className={chat.body ? style.userChatDiv : style.none}>
+                                    {chat.body}
+                                </div>
+                                <div className={chat.answer ? style.chatBotChatFrame : style.none}>
+                                    <img src="/img/chatchat.png" className={chat.answer ? style.chatBotChatIcon : style.none}/>
+                                    <div className={chat.answer ? style.chatBotChattingDiv : style.none}>
+                                        {chat.answer}
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    )
+                    )}
+                    </ul>
+                </div>
+                
             </div>
             <div className={style.bottomFrame}>
             <input type="text" ref={chat} placeholder="척추의 요정 레톤이에게 무엇이든 물어보세요!" className={style.chat}></input>
--- spinetracker60/src/webcamPage/component/utils/ChatBot.js
+++ spinetracker60/src/webcamPage/component/utils/ChatBot.js
@@ -1,43 +1,14 @@
-import { io } from "socket.io-client";
+
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { API_BASE_URL_SOCKET, API_BASE_URL_SOCKET2 } from "../../../constants";
 
-
-
-
-    let socket;
-    const ENDPOINT = API_BASE_URL_SOCKET2;
-    
-    
-    socket = io(`${ENDPOINT}/room`, {
-      transports: ["websocket"]
-    });
-
-    socket.connect();
-
-    socket.on("join", (chat) => {
-      console.log(chat);
-    });
-
-    socket.on("chat", (chat) => {
-      console.log(chat);
-    });
-
-
-const ChatBot = (props) => {
-    const [room, setRoom] = useState("");
-    const [chats, setChats] = useState([]);
-    
-
-    useEffect(() =>{
-
-      if(props.user){
-      const userId = props.user.id;
+export const roomJoin = (userId, socket) => {
+  ///const userId = props.user.id;
       axios.get(API_BASE_URL_SOCKET + '/room?memberId=' + userId)
       .then(response => {
         const roomId = response.data.body.roomId;
-        console.log(roomId);
+        // console.log(roomId);
         socket.emit("room-join", {roomId});
       })
       .catch(function(error){
@@ -45,27 +16,58 @@ const ChatBot = (props) => {
           axios.post(API_BASE_URL_SOCKET + '/room?memberId=' + userId)
           .then(response => {
             const roomId = response.data.body.roomId;
-            console.log(roomId);
+            // console.log(roomId);
             socket.emit("room-join", {roomId});
           })
         }
       })
-    }
-    }
-    ,
-    [])
+} 
+
+
+    
+
+
+// const ChatBot = (props) => {
+//     const [room, setRoom] = useState("");
+//     const [chats, setChats] = useState([]);
+    
+
+//     useEffect(() =>{
+
+//       if(props.user){
+//       const userId = props.user.id;
+//       axios.get(API_BASE_URL_SOCKET + '/room?memberId=' + userId)
+//       .then(response => {
+//         const roomId = response.data.body.roomId;
+//         // console.log(roomId);
+//         socket.emit("room-join", {roomId});
+//       })
+//       .catch(function(error){
+//         if(error.message == "Request failed with status code 404"){
+//           axios.post(API_BASE_URL_SOCKET + '/room?memberId=' + userId)
+//           .then(response => {
+//             const roomId = response.data.body.roomId;
+//             // console.log(roomId);
+//             socket.emit("room-join", {roomId});
+//           })
+//         }
+//       })
+//     }
+//     }
+//     ,
+//     [])
     
     
 
-    useEffect(() => {
-    // 서버에서 message 이벤트가 올 경우에 대해서 `on`
+//     useEffect(() => {
+//     // 서버에서 message 이벤트가 올 경우에 대해서 `on`
 
-    }, [chats]);
+//     }, [chats]);
 
 
 
-    // console.log(message, messages);
-    // console.log(users, "users");
-};
+//     // console.log(message, messages);
+//     // console.log(users, "users");
+// };
 
-export default ChatBot;
+// export default ChatBot;
