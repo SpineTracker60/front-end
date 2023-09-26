3955fa1cd19dc4cbc5fe21fd842507fea89cdad4
anichan <qufekf97@gmail.com>
2023-09-26 19:02:56 +0900

    대시보드 화면 구성

--- spinetracker60/src/webcamPage/component/Chart/Chart.js
+++ spinetracker60/src/webcamPage/component/Chart/Chart.js
@@ -5,7 +5,8 @@ function Chart(){
 
     return(
         <div className={style.frameDiv}>
-            
+            <p className={style.dashboard2}>대시보드</p>
+            <img src="/img/dashboard.png" className={style.dashboard}></img>
         </div>
         
     )
